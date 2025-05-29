bfb81142decb40ba92c2fcd95ee824a92796dc29
grimmjowESP6 <mail2arshad113@gmail.com>
2025-05-29 13:29:06 +0530

    Made the form method to post, to hide the submission details

--- DTL/Design/views.py
+++ DTL/Design/views.py
@@ -9,7 +9,7 @@ def welcome(request):
     return render(request, 'home.html', content)
 
 def add(request):
-    num1 = int(request.GET['number1'])
-    num2 = int(request.GET['number2'])
+    num1 = int(request.POST['number1'])
+    num2 = int(request.POST['number2'])
     res = num1+num2
     return render(request, 'result.html',{'result':res})
